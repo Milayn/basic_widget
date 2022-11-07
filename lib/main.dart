1bad730a7036a25a164d3c06b5ab3f7ffe93527e
Milayn <millayunita07@gmail.com>
2022-11-07 10:18:20 +0700

    basic_widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,109 +8,122 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  Row ListViewStyle(String url, String keterangan) {
+    return Row(children: <Widget>[
+      Container(
+        color: Colors.deepOrange,
+        padding: const EdgeInsets.only(left: 30.0),
+        child: Image(
+          width: 120,
+          height: 120,
+          image: NetworkImage(url),
+          fit: BoxFit.fitWidth,
+        ),
+      ),
+      Container(
+        margin: const EdgeInsets.symmetric(vertical: 5.0),
+        color: Colors.deepOrange,
+        alignment: Alignment.center,
+        height: 120.0,
+        width: 310.0,
+        child: Text(keterangan),
+      )
+    ]);
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepOrange,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("My Apps"),
+        ),
+        body: ListView(
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.all(16.0),
+                  child: const Text("BERITA TERBARU",
+                      style: TextStyle(fontSize: 15)),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(16.0),
+                  child: const Text("PERTANDINGAN HARI INI",
+                      style: TextStyle(fontSize: 15)),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Container(
+              height: 200.0,
+              child: GridView.count(
+                crossAxisSpacing: 5.0,
+                crossAxisCount: 5,
+                childAspectRatio: 9 / 24,
+                children: <Widget>[
+                  Image.network(
+                    "https://t-2.tstatic.net/bengkulu/foto/bank/images/Kylian-Mbappe-Pernah-Berseteru-dengan-Neymar-Peran-Lionel-Messi-Menengahi-Perselisihan.jpg",
+                    fit: BoxFit.fill,
+                  ),
+                  Image.network(
+                    "https://img.okezone.com/okz/500/library/images/2022/10/29/5v452n82krttsggh9vak_19143.JPG",
+                    fit: BoxFit.fill,
+                  ),
+                  Image.network(
+                    "https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/bltd8fd9c190cff02da/6329018a6ced0010ca9f0b87/Cristiano_Ronaldo_Portugal_2022.png",
+                    fit: BoxFit.fill,
+                  ),
+                  Image.network(
+                    "https://cdns.klimg.com/bola.net/library/upload/21/2019/05/645x430/david-beckham_13d3d75.jpg",
+                    fit: BoxFit.fill,
+                  ),
+                  Image.network(
+                    "https://cdns.klimg.com/bola.net/library/upload/20/2016/03/kaka_9fca1e4.jpg",
+                    fit: BoxFit.fill,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(15.0),
+              child: const Text(
+                "Lima Pesepak Bola yang Terkenal Dermawan",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              height: 650.0,
+              child:
+                  ListView(scrollDirection: Axis.vertical, children: <Widget>[
+                ListViewStyle(
+                    "https://t-2.tstatic.net/bengkulu/foto/bank/images/Kylian-Mbappe-Pernah-Berseteru-dengan-Neymar-Peran-Lionel-Messi-Menengahi-Perselisihan.jpg",
+                    "1. Kylian Mbappe"),
+                ListViewStyle(
+                    "https://img.okezone.com/okz/500/library/images/2022/10/29/5v452n82krttsggh9vak_19143.JPG",
+                    "2. Lionel Messi"),
+                ListViewStyle(
+                    "https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/bltd8fd9c190cff02da/6329018a6ced0010ca9f0b87/Cristiano_Ronaldo_Portugal_2022.png",
+                    "3. Christiano Ronaldo"),
+                ListViewStyle(
+                    "https://cdns.klimg.com/bola.net/library/upload/21/2019/05/645x430/david-beckham_13d3d75.jpg",
+                    "4. David Beckham"),
+                ListViewStyle(
+                    "https://cdns.klimg.com/bola.net/library/upload/20/2016/03/kaka_9fca1e4.jpg",
+                    "5. Ricardo Kaka")
+              ]),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
